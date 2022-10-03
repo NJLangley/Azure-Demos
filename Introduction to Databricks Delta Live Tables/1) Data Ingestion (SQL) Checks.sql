9666c69e4f823311210e3ba4c8f5f5d428acb11d
niall.langley@gmail.com <niall.langley@gmail.com>
2022-10-03 14:49:38 +0000

    Working demos 2 & 3

---
+++ Introduction to Databricks Delta Live Tables/1) Data Ingestion (SQL) Checks.sql
@@ -0,0 +1,162 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ## Customer SCD tables
+
+-- COMMAND ----------
+
+SELECT *
+FROM adventure_works_demo_1.Bronze_Customer
+WHERE CustomerId = 6
+ORDER BY ModifiedDate
+
+-- COMMAND ----------
+
+SELECT *
+FROM adventure_works_demo_1.Silver_Customer_No_SCD
+WHERE CustomerId = 6
+
+-- COMMAND ----------
+
+SELECT *
+FROM adventure_works_demo_1.Silver_Customer_SCD1
+WHERE CustomerId = 6
+
+-- COMMAND ----------
+
+SELECT *
+FROM adventure_works_demo_1.Silver_Customer_SCD2
+WHERE CustomerId = 6
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Views
+
+-- COMMAND ----------
+
+-- This does not exist as it was a view
+SELECT *
+FROM adventure_works_demo_1.Bronze_ProductModel
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Silver tables
+
+-- COMMAND ----------
+
+SELECT *
+FROM adventure_works_demo_1.Bronze_SalesOrderHeader
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## DLT Events
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMPORARY VIEW event_log_raw
+AS
+SELECT *
+FROM DELTA.`abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/DLT-pipelines/adventure_works_demo_1/system/events`
+
+-- COMMAND ----------
+
+SELECT *
+FROM event_log_raw
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC This is a sample query from the Databricks docs to show the data quality from the last run. 
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMPORARY VIEW latest_update_id
+AS
+SELECT origin.update_id
+FROM event_log_raw
+WHERE event_type = 'create_update' 
+ORDER BY timestamp DESC
+LIMIT 1
+
+-- COMMAND ----------
+
+SELECT update_id
+FROM latest_update_id
+LIMIT 1
+
+-- COMMAND ----------
+
+SELECT
+  row_expectations.dataset as dataset,
+  row_expectations.name as expectation,
+  SUM(row_expectations.passed_records) as passing_records,
+  SUM(row_expectations.failed_records) as failing_records
+FROM
+  (
+    SELECT
+      explode(
+        from_json(
+          details :flow_progress :data_quality :expectations,
+          "array<struct<name: string, dataset: string, passed_records: int, failed_records: int>>"
+        )
+      ) row_expectations
+    FROM
+      event_log_raw
+    WHERE
+      event_type = 'flow_progress'
+      AND origin.update_id = (SELECT update_id FROM latest_update_id LIMIT 1)
+  )
+GROUP BY
+  row_expectations.dataset,
+  row_expectations.name
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC Here's a query to get the number of rows updated/dropped on each table in the last run
+
+-- COMMAND ----------
+
+SELECT
+  flow_name as TableName,
+  metrics.num_output_rows,
+  dropped_records.dropped_records
+FROM
+  (
+    SELECT
+      origin.flow_name,
+      from_json(
+        details :flow_progress :metrics, "struct<num_output_rows :int>"
+      ) as metrics,
+      from_json(
+        details :flow_progress :data_quality, "struct<dropped_records :int>"
+      ) as dropped_records
+    FROM
+      event_log_raw
+    WHERE
+      event_type = 'flow_progress'
+      AND origin.update_id = (SELECT update_id FROM latest_update_id LIMIT 1)
+  )
+WHERE metrics.num_output_rows IS NOT NULL
+ORDER BY
+   flow_name
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC Changing the query to add more columns does not update old records without a reload
+
+-- COMMAND ----------
+
+select * from adventure_works_demo_1.Silver_Product_Enriched
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Drop the DB if we want to start from scratch
+
+-- COMMAND ----------
+
+-- DROP DATABASE adventure_works_demo_1 CASCADE
---
+++ Introduction to Databricks Delta Live Tables/1) Data Ingestion (SQL).sql
@@ -0,0 +1,250 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC # Demo 1: Data Ingestion Using SQL
+-- MAGIC ##### We can load data into delta live tables using either SQL or Python Notebook. This one is a SQL notebook.
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Bronze / Ingestion tables
+
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE TABLE Bronze_Product
+TBLPROPERTIES ("layer" = "bronze")
+AS SELECT *,
+          input_file_name() as FileName
+   FROM cloud_files("abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/landed/csv/Product", 
+                    "csv",
+                    map(
+                      "delimiter", "|",
+                      "header", "true",
+                      "inferSchema", "true"
+                    )
+                   );
+
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE TABLE Bronze_ProductCategory
+COMMENT "The Adventure Works bronze product category table ingested from landed data"
+TBLPROPERTIES ("layer" = "bronze")
+AS SELECT *,
+          input_file_name() as FileName
+   FROM cloud_files("abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/landed/csv/ProductCategory", 
+                    "csv",
+                    map(
+                      "delimiter", "|",
+                      "header", "true",
+                      "inferSchema", "true"
+                    )
+                   );
+
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE TABLE Bronze_Customer
+COMMENT "The Adventure Works bronze customer table ingested from landed data"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/bronze/Customer"
+TBLPROPERTIES ("layer" = "bronze")
+AS SELECT *,
+          input_file_name() as FileName
+   FROM cloud_files("abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/landed/csv/Customer", 
+                    "csv",
+                    map(
+                      "delimiter", "|",
+                      "header", "true",
+                      "pathGlobFilter", "Customer_[1-2].csv",
+                      "inferSchema", "true"
+                    )
+                   );
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### We can also create delta live views. These are not real views in the target DB, but more like intermediate stages in the ETL process.
+
+-- COMMAND ----------
+
+-- View's only exist as an intermediate step in a DLT pipeline. We cannot query them after the pipeline has run 
+CREATE INCREMENTAL LIVE VIEW Bronze_SalesOrderHeader
+TBLPROPERTIES ("layer" = "bronze")
+AS SELECT *,
+          input_file_name() as FileName
+   FROM cloud_files("abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/landed/csv/SalesOrderHeader", 
+                    "csv",
+                    map(
+                      "delimiter", "|",
+                      "header", "true",
+                      "inferSchema", "true"
+                    )
+                   );
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### We can also create a live table directly on a file using SQL syntax
+-- MAGIC This table is not incremental, meaning it and it's dependencies are fully refreshed every run.
+-- MAGIC 
+-- MAGIC Unfortunately for CSV we can't use custom delimiters without creating a normal table first, and that is not supported in a DLT notebook. We can do this for parquet, delta and JSON data. The path must be surronded by backticks, not quotes.
+
+-- COMMAND ----------
+
+CREATE LIVE TABLE Bronze_ProductModel
+COMMENT "The Adventure Works bronze product model table ingested from landed data"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/bronze/ProductModel"
+TBLPROPERTIES ("layer" = "bronze")
+AS SELECT *,
+          input_file_name() as FileName
+   FROM parquet.`abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/landed/parquet/ProductModel/`
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ## Silver / Enriched tables
+-- MAGIC ### Data Quality
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC > ***Note:*** *The query below does not work as a left join, as this would require a watermark on each side of the join to ensure late arriving data is handled correclty. Changing to an inner join works, but then the `ProductCategory_Is_Not_Null` data qaulity check is broken as records will get dropped in the join if the product category is not found.*
+
+-- COMMAND ----------
+
+CREATE INCREMENTAL LIVE TABLE Silver_SalesOrderHeader (
+  CONSTRAINT Status_Not_Equal_To_99 EXPECT ( Status <> 99 ),
+  CONSTRAINT Total_is_Correct EXPECT ( (SubTotal + TaxAmt + Freight) = TotalDue ) ON VIOLATION DROP ROW
+)
+COMMENT "The Adventure Works silver product table ingested from landed data"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/silver/SalesOrderHeader"
+TBLPROPERTIES ("layer" = "silver")
+AS SELECT *
+   FROM STREAM (live.Bronze_SalesOrderHeader)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC The next table is not incremental, so is re-loaded each time the DLT workflow runs. It is reading from a incremental source, but as the target is not incremental we don't ad the `STREAMING()` wrapper to the source table
+
+-- COMMAND ----------
+
+CREATE LIVE TABLE Silver_ProductCategory
+COMMENT "The Adventure Works silver product category ingested from landed data"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/silver/ProductCategory"
+TBLPROPERTIES ("layer" = "silver")
+AS SELECT *
+   FROM live.Bronze_ProductCategory
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### We can join tables too
+-- MAGIC There are some limits on join types though due to DLT being built on top of structured streaming
+
+-- COMMAND ----------
+
+-- This table is not incremental, so is fully loaded each time the pipeline runs
+CREATE INCREMENTAL LIVE TABLE Silver_Product_Enriched (
+  CONSTRAINT SellStartDate_Is_Not_Null EXPECT (SellStartDate IS NOT NULL) ON VIOLATION DROP ROW,
+  CONSTRAINT ProductCategory_Is_Not_Null EXPECT (ProductCategory IS NOT NULL)
+)
+COMMENT "The Adventure Works silver product table ingested from landed data"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/silver/Product"
+TBLPROPERTIES ("layer" = "silver")
+AS SELECT
+     p.ProductID
+    ,p.Name
+    ,p.ProductNumber
+    ,p.Color
+    ,p.StandardCost
+    ,p.ListPrice
+    ,p.Size
+    ,p.Weight
+    ,p.SellStartDate
+    ,p.SellEndDate
+    ,p.DiscontinuedDate
+    ,pc.Name as ProductCategory
+--     ,pm.Name as ProductModel
+   -- Join a live view and table together
+   FROM STREAM(live.Bronze_Product) AS p
+   -- Left joins are not supported for two live tables without adding a watermark in the join keys, or a watermark on the nullable side 
+   -- and an appropriate range condition. This is a restriction inherited from spark structured streaming.
+--    LEFT JOIN STREAM(live.Bronze_ProductCategory) AS pc
+   -- Change to inner to make this run
+   INNER JOIN STREAM(live.Bronze_ProductCategory) AS pc
+     ON pc.ProductCategoryID = p.ProductCategoryID
+--    LEFT JOIN live.Bronze_ProductModel AS pm
+--      ON pm.ProductModelID = p.ProductModelID
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC > ***Note:*** *In the example above we can't join to the non-incremental table Silver_ProductCategory instead because we get a different error. In short the source(s) for an incremental table must be append only. The non-incremental source is replaced each time, and it knows that because the version counter on the Silver_ProductCategory delta table increments each time we re-run the DLT workflow*
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### CDC (Merge/Upsert)
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### Without using the CDC feature
+
+-- COMMAND ----------
+
+-- Without using the APPLY CHANGES INTO syntax, no deduplication happens
+CREATE INCREMENTAL LIVE TABLE Silver_Customer_No_SCD
+COMMENT "The Adventure Works silver customer table using the SCD1 merge type"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/silver/Customer_No_SCD"
+TBLPROPERTIES ("layer" = "silver")
+AS SELECT *
+   FROM STREAM(live.Bronze_Customer);
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### SCD1
+
+-- COMMAND ----------
+
+-- Incremental means the table is a streaming table, and we incrementlly add the changes to it
+CREATE INCREMENTAL LIVE TABLE Silver_Customer_SCD1
+COMMENT "The Adventure Works silver customer table using the SCD1 merge type"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/silver/Customer_SCD1"
+TBLPROPERTIES ("layer" = "silver");
+
+-- COMMAND ----------
+
+APPLY CHANGES INTO LIVE.Silver_Customer_SCD1
+FROM STREAM(live.Bronze_Customer)
+KEYS (CustomerID)
+-- [WHERE condition] -- Use for partition pruning
+-- [IGNORE NULL UPDATES]
+-- [APPLY AS DELETE WHEN condition]
+-- [APPLY AS TRUNCATE WHEN condition]
+SEQUENCE BY ModifiedDate
+-- [COLUMNS {columnList | * EXCEPT (exceptColumnList)}]
+COLUMNS * EXCEPT (PasswordHash, PasswordSalt, rowguid)
+-- [STORED AS {SCD TYPE 1 | SCD TYPE 2}]
+STORED AS SCD TYPE 1
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ##### SCD2
+
+-- COMMAND ----------
+
+-- Incremental means the table is a streaming table, and we incrementlly add the changes to it
+CREATE INCREMENTAL LIVE TABLE Silver_Customer_SCD2
+COMMENT "The Adventure Works silver customer table using the SCD2 merge type"
+LOCATION "abfss://demo-lake@puddle.dfs.core.windows.net/adventure-works/Demo1/silver/Customer_SCD2"
+TBLPROPERTIES ("layer" = "silver");
+
+-- COMMAND ----------
+
+APPLY CHANGES INTO LIVE.Silver_Customer_SCD2
+FROM STREAM(live.Bronze_Customer)
+KEYS (CustomerID)
+SEQUENCE BY ModifiedDate
+COLUMNS * EXCEPT (PasswordHash, PasswordSalt, rowguid)
+STORED AS SCD TYPE 2
